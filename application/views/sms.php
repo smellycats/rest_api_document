1041e5c747bbc361d4e8b7452ce7cca81ff9a06e
SmellyCat <smellycat2014@foxmail.com>
2015-11-07 18:59:31 +0800

    Add sms

---
+++ application/views/sms.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
+    <title><?php echo $this->config->item('title'); ?></title>
+
+    <!-- Bootstrap -->
+    <link href="<?php echo base_url('bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
+    <link href="<?php echo base_url('bootstrap/css/bootstrap-theme.min.css'); ?>" rel="stylesheet">
+    <link href="<?php echo base_url('bootstrap/css/docs.min.css'); ?>" rel="stylesheet">
+
+    <link href="<?php echo base_url('highlight/styles/github.css'); ?>" rel="stylesheet">
+
+    <link href="<?php echo base_url('font-awesome/css/font-awesome.min.css'); ?>" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="//cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="//cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="<?php echo base_url('jquery/jquery-1.11.3.min.js'); ?>"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?php echo base_url('bootstrap/js/bootstrap.min.js'); ?>"></script>
+    <script src="<?php echo base_url('bootstrap/js/docs.min.js'); ?>"></script>
+
+    <script src="<?php echo base_url('highlight/highlight.pack.js'); ?>"></script>
+    <script>hljs.initHighlightingOnLoad();</script>
+  </head>
+
+  <body>
+
+    <!-- Docs master nav -->
+    <header class="navbar navbar-static-top bs-docs-nav" id="top" role="banner">
+    <div class="container">
+      <div class="navbar-header">
+        <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#bs-navbar" aria-controls="bs-navbar" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a href="<?php echo site_url(); ?>" class="navbar-brand">Documents</a>
+      </div>
+      <nav id="bs-navbar" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <?php foreach($this->config->item('menu') as $row): ?>
+          <li <?php if ($row['name'] == 'cgs'){echo 'class="active"'; }?>>
+            <a href="<?php echo site_url($row['name']); ?>"><?php echo $row['cname']; ?></a>
+          </li>
+          <?php endforeach; ?>
+        </ul>
+      </nav>
+    </div>
+    </header>
+
+    <!-- Docs page layout -->
+    <div class="bs-docs-header" id="content" tabindex="-1">
+      <div class="container">
+        <h1>短信服务</h1>
+        <p>短信发送服务</p>
+        <p class="lead">
+          <a class="btn btn-outline-inverse btn-lg" href="https://github.com/smellycats/SX-SMSServer" role="button">
+            <i class="fa fa-github fa-lg"></i>
+            GitHub
+          </a>
+        </p>
+        <p style="font-size:14px;">Version 0.1.0 . Created by SmellyCat</p>
+      </div>
+    </div>
+
+    <div class="container bs-docs-container">
+      <div class="row">
+        <div class="col-md-8" role="main">
+          <div class="bs-docs-section">
+            <h1 id="sms" class="page-header">短信服务</h1>
+
+              <h2 id="sms-sms">Create a sms</h2>
+                <p>发送短信<p>
+                <div class="panel panel-default">
+                  <div class="panel-body">
+                    POST /sms
+                  </div>
+                </div>
+                <h3 id="package-create-parameters">Parameters</h3>
+                  <table class="table table-bordered">
+                    <colgroup>
+                      <col class="col-xs-1">
+                      <col class="col-xs-1">
+                      <col class="col-xs-6">
+                    </colgroup>
+                    <thead>
+                      <tr>
+                        <th>Name</th>
+                        <th>Type</th>
+                        <th>Description</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>mobiles</td>
+                        <td>array of string</td>
+                        <td>要发送的手机号码.</td>
+                      </tr>
+                      <tr>
+                        <td>content</td>
+                        <td>string</td>
+                        <td>发送内容.</td>
+                      </tr>
+                      <tr>
+                        <td>smid</td>
+                        <td>boolean</td>
+                        <td>True表示发送id递增，有回执的短信，反之亦然.</td>
+                      </tr>
+                  </table>
+                <h3 id="package-create-input">Input</h3>
+                  <div>
+<pre><code class="json">
+{
+  "mobiles": [
+    "10086",
+    "709394"
+  ],
+  "content": "短信测试",
+  "smid": True
+}
+</code></pre>
+                  </div>
+                <h3 id="package-create-response">Response</h3>
+                  <div class="panel panel-default">
+                    <div class="panel-heading">Status: 201 Created</div>
+                      <div>
+<pre><code class="json">
+{
+  "id": 8,
+  "mobiles": [
+    "10086",
+    "709394"
+  ],
+  "date_send": "2015-11-01 12:34:56",
+  "content": "短信测试",
+  "user_id": 2,
+  "returned_value": 0
+}
+</code></pre>
+                      </div>
+                  </div>
+          </div>
+        </div>
+
+        <div class="col-md-4" role="complementary">
+          <nav class="bs-docs-sidebar hidden-print hidden-xs hidden-sm">
+            <ul class="nav bs-docs-sidenav">
+              <li>
+                <a href="#sms">短信服务</a>
+                <ul class="nav">
+                  <li><a href="#sms-sms">Create a sms</a></li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+    <!-- Footer================================================== -->
+    <footer class="bs-docs-footer" role="contentinfo">
+      <div class="container">
+
+        <ul class="bs-docs-footer-links text-muted">
+          <li>当前版本： <?php echo $this->config->item('version'); ?></li>
+          <li>&middot;</li>
+          <li><a href="https://github.com/smellycats">GitHub 仓库</a></li>
+        </ul>
+      </div>
+    </footer>
+  </body>
+</html>
