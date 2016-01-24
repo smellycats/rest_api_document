22eee21db1c3e0481c4af143de2f2b044240bed4
SmellyCat <smellycat2014@foxmail.com>
2016-01-24 23:01:03 +0800

    增加 sms.php

---
+++ application/Sms.php
@@ -0,0 +1,33 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Connecting
+ *
+ * This is a connecting document
+ *
+ * @package     CodeIgniter
+ * @subpackage  Connecting Rest Server
+ * @category    Controller
+ * @author      Fire
+*/
+
+
+class SMS extends Admin_Controller
+{
+    public function __construct()
+    {
+        // Construct our parent class
+        parent::__construct();
+    }
+
+    /**
+     * 短信服务 index
+     * 
+     * @return void
+     */
+    public function index()
+    {
+        $this->load->view('sms');
+    }
+
+}
