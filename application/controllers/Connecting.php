571cb53ba1f63925cb8d55f2a4ca448377e0df80
SmellyCat <smellycat2014@foxmail.com>
2016-01-24 23:27:19 +0800

    更新 控制器文件名转成首字母大写

---
+++ application/controllers/Connecting.php
@@ -0,0 +1,28 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Connecting
+ *
+ * This is a connecting document
+ *
+ * @package     CodeIgniter
+ * @subpackage  Connecting Rest Server
+ * @category    Controller
+ * @author      Fire
+*/
+
+
+class Connecting extends Admin_Controller
+{
+    public function __construct()
+    {
+        // Construct our parent class
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $this->load->view('connecting');
+    }
+
+}
